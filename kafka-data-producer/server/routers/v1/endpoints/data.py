ed362cb5b19400a798995e270869df14805600a0
lisy09 <lisy09.thu@gmail.com>
2021-01-01 17:50:37 +0900

    krb5 for kafka-data-producer

--- kafka-data-producer/server/routers/v1/endpoints/data.py
+++ kafka-data-producer/server/routers/v1/endpoints/data.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,status, Request
 from fastapi.logger import logger as fastapi_logger
 from server.core.config import get_config, get_kerberos_config, get_use_kerberos_config
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer
 from pydantic import BaseModel
 
 import logging
@@ -30,16 +30,8 @@ async def send_kafka_data(data:KafkaData):
             sasl_kerberos_service_name=krb5Config["kafkaServiceName"],
             client_id="kafka-clients"
         )
-        consumer = KafkaConsumer(bootstrap_servers=kafkaConfig["brokers"],
-            security_protocol='SASL_PLAINTEXT',
-            sasl_mechanism='GSSAPI',
-            sasl_kerberos_domain_name=krb5Config["domain"],
-            sasl_kerberos_service_name=krb5Config["kafkaServiceName"],
-        )
     else:
         producer = KafkaProducer(bootstrap_servers=kafkaConfig["brokers"])
-    t = consumer.topics()
-    fastapi_logger.debug(t)
     producer.send(kafkaConfig["targetTopic"], data.msg.encode('utf-8'))
     producer.flush()
     fastapi_logger.debug(f"send message:{data.msg}")
